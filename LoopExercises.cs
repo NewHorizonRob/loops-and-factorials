a998b541260cd3478a08f897800cfb784d9e9e53
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-23 17:53:07 -0400

    Add files via upload

---
+++ LoopExercises.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5Assignment
+{
+    class LoopExercises
+    {
+        public void DrawBox(int s)
+        {
+             int y = 0;
+            while (y < s)
+            {
+                y++;
+             int x = 0;
+
+                
+                while (x < s)
+                {
+                    Console.Write("*");
+                    x++;
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public void GetFactorial(int l)
+        {
+            int value = 1;
+            int i = 1;
+            while(i <= l)
+            {
+                value = value * i;
+                i++;
+            Console.WriteLine($"{value}");
+
+            }
+
+
+        }
+
+      /*  public void ShowE(int precision)
+        {
+            //E is 1+(1/n!) + (1/n!) + (1/ (n+!)!)+.... 
+            double runningTotal = 1;
+
+            int n = 0;
+            while(n < precision)
+            {
+                n++;
+
+                runningTotal = runningTotal + (1 / (GetFactorial(n)));
+            }
+            Console.WriteLine($"E with a precision of {precision} is {runningTotal}");
+        } */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopExercises h = new LoopExercises();
+            h.DrawBox(7);
+          h.GetFactorial(4);
+          //  h.ShowE(2);
+            Console.WriteLine("PAK");
+            Console.ReadKey();
+        }
+    }
+}
