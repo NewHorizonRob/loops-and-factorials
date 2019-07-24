ba729d5a405dfef060dac8194fb597f943ccd586
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 19:04:48 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace Ch5Assignment
     {
         static void Main(string[] args)
         {
-            LoopExercises h = new LoopExercises();
-            h.DrawBox(7);
+            LoopExercises h = new LoopExercises(); #initializes a new instance from the LoopExercises class
+          h.DrawBox(7); #each of these are method calls from the LoopExercises class used in this class 
           h.GetFactorial(4);
           //  h.ShowE(2);
             Console.WriteLine("PAK");
