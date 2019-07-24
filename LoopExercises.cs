dc7df9ac680d2e50f4d13c0deea96958cc823f50
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 19:01:50 -0400

    Update LoopExercises.cs

--- LoopExercises.cs
+++ LoopExercises.cs
@@ -8,19 +8,19 @@ namespace Ch5Assignment
 {
     class LoopExercises
     {
-        public void DrawBox(int s)
+        public void DrawBox(int s) #method for drawing a box with an integer as a parameter
         {
-             int y = 0;
-            while (y < s)
+             int y = 0; #sets the integer to 0
+            while (y < s) #while loop
             {
-                y++;
-             int x = 0;
+                y++; #increments the y variable 
+             int x = 0; #sets the x variable to 0
 
                 
-                while (x < s)
+                while (x < s) #while loop x<s
                 {
-                    Console.Write("*");
-                    x++;
+                    Console.Write("*"); #writes the character out to the console
+                    x++; #increments the x variable 
 
                 }
                 Console.WriteLine();
@@ -28,15 +28,15 @@ namespace Ch5Assignment
 
         }
 
-        public void GetFactorial(int l)
+        public void GetFactorial(int l) #method for the factorial with an integer as a parameter
         {
-            int value = 1;
-            int i = 1;
-            while(i <= l)
+            int value = 1; #sets the value variable to 1
+            int i = 1; #sets the i variable to 1
+            while(i <= l) #while loop (while i is < l)
             {
-                value = value * i;
+                value = value * i; #multiplies i and the value to create a new product for value
                 i++;
-            Console.WriteLine($"{value}");
+            Console.WriteLine($"{value}"); #writes out the value to the console
 
             }
 
